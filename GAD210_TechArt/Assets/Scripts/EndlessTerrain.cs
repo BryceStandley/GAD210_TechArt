a0a4d21d1b4f8d7fe79fa942b6f4ae7481cb4b0e
Bryce Standley <bryce@vectorpixel.net>
2019-11-15 12:23:31 +0800

    Ep 8
    Made it to ep 8, threading finished

---
+++ GAD210_TechArt/Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 300f;
+    public Transform viewer;
+    public Material mapMaterial;
+
+    static MapGenerator mapGenerator;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunksVisableLastUpdate = new List<TerrainChunk>();
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunkVisableInViewDistance;
+
+    void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunkVisableInViewDistance = Mathf.RoundToInt(maxViewDistance / chunkSize);
+        mapGenerator = FindObjectOfType<MapGenerator>();
+    }
+
+    private void Update() {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisableChunks();
+    }
+    void UpdateVisableChunks()
+    {
+        for(int i = 0; i < terrainChunksVisableLastUpdate.Count; i++)
+        {
+            terrainChunksVisableLastUpdate[i].SetVisable(false);
+        }
+        terrainChunksVisableLastUpdate.Clear();
+
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for(int yOffset = -chunkVisableInViewDistance; yOffset <= chunkVisableInViewDistance; yOffset++)
+        {
+            for(int xOffset = -chunkVisableInViewDistance; xOffset <= chunkVisableInViewDistance; xOffset++)
+            {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+                if(terrainChunkDictionary.ContainsKey(viewedChunkCoord))
+                {
+                    terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
+                    if(terrainChunkDictionary[viewedChunkCoord].IsVisible())
+                    {
+                        terrainChunksVisableLastUpdate.Add(terrainChunkDictionary[viewedChunkCoord]);
+                    }
+                }
+                else
+                {
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, transform, mapMaterial));
+                }
+            }
+        }
+    }
+
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+        public TerrainChunk(Vector2 coord, int size, Transform parent, Material material)
+        {
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+            meshObject = new GameObject("TerrainChunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshRenderer.material = material;
+
+            meshObject.transform.position = positionV3;
+            meshObject.transform.parent = parent;
+            SetVisable(false);
+            mapGenerator.RequestMapData(OnMapDataReceived);
+        }
+
+        void OnMapDataReceived(MapData mapData)
+        {
+            mapGenerator.RequestMeshData(mapData, OnMeshDataReceived);
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            meshFilter.mesh = meshData.CreateMesh();
+        }
+
+        public void UpdateTerrainChunk() {
+            {
+                float viewerDistanceFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+                bool visible = viewerDistanceFromNearestEdge <= maxViewDistance;
+                SetVisable(visible);
+            }
+        }
+
+        public void SetVisable(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
